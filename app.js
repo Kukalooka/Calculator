e3741202888e22fa915c382842f5f1401bbde5e2
Kukalooka <kukalkapol@gmail.com>
2021-09-15 20:31:23 +0200

    Removed the useless Number conversion

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ import { quad } from "./square.js";
 let button = document.querySelector("#butt");
 
 button.addEventListener("click", function(){
-    let a = Number(document.querySelector("#one").value);
-    let b = Number(document.querySelector("#two").value);
-    let c = Number(document.querySelector("#three").value);
+    let a = document.querySelector("#one").value;
+    let b = document.querySelector("#two").value;
+    let c = document.querySelector("#three").value;
 
     console.log(quad(a,b,c));
 });

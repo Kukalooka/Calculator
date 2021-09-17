a9dbdd176a4378d8c155386279e3e31bc5a794e7
Kukalooka <kukalkapol@gmail.com>
2021-09-17 03:47:48 +0200

    Added new module for normal calculations

---
+++ normalcalc.js
@@ -0,0 +1,13 @@
+function calc(mode, a, b)
+{
+    switch(mode){
+        case "add":
+            return a+b;
+        case "substract":
+            return a-b;
+        case "multiply":
+            return a*b;
+        case "divide":
+            return a/b;
+    }
+}
